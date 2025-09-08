09f92c0bfc7e976f97390ffce7e50f9e0cf44e5d
John Pork <kylew2006@gmail.com>
2025-09-08 13:59:13 +1000

    inital project with from start.spring.io using maven w springboot v 3.5.5 using jar on java version 21. dependencies are web, jpa & h2

---
+++ src/main/java/com/group18/rotionary/RotionaryApplication.java
@@ -0,0 +1,13 @@
+package com.group18.rotionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group18/rotionary/RotionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group18.rotionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
